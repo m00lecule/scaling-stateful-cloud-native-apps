f551f8dd360bde0d44c0ba7a32fca9b674a3efd5
Michal Dygas <michal.dygas@shijigroup.com>
2022-08-24 00:04:00 +0200

    feat(app): correct healtcheck

--- app/handlers/health.go
+++ app/handlers/health.go
@@ -4,9 +4,22 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	config "github.com/m00lecule/stateful-scaling/config"
 )
 
 func GetHealth(c *gin.Context) {
+	if config.Meta.IsStateful {
+		ctx := c.Request.Context()
+		_, err := config.RDB.Ping(ctx).Result()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"Error": err,
+			})
+			return
+		}
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"status": "UP",
 	})
